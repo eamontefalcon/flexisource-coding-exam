c8e81df96e2abced9182a73be7d10b1d690f039d
Eliseo Montefalcon <eliseo.montefalcon@gmail.com>
2024-01-18 11:41:46 +0800

    remove destructuring array

--- app/DTO/GetCustomerDTO.php
+++ app/DTO/GetCustomerDTO.php
@@ -19,46 +19,20 @@ class GetCustomerDTO
     {
         // Process each user in the results array
         foreach ($results as $user) {
-            //destructure data from api response
-            /**
-             * We can use the other variable here later if we needed so other developers can easily add without
-             * checking the api response in actual or checking the docs
-            **/
-            [
-                'gender' => $gender,
-                'name' => ['title' => $title, 'first' => $first, 'last' => $last],
-                'location' => [
-                    'street' => ['number' => $streetNumber, 'name' => $streetName],
-                    'city' => $city,
-                    'state' => $state,
-                    'country' => $country,
-                    'postcode' => $postcode,
-                    'coordinates' => ['latitude' => $latitude, 'longitude' => $longitude],
-                    'timezone' => ['offset' => $timezoneOffset, 'description' => $timezoneDescription],
-                ],
-                'email' => $email,
-                'login' => [
-                    'uuid' => $uuid,
-                    'username' => $username,
-                    'password' => $password,
-                    'salt' => $salt,
-                    'md5' => $md5,
-                    'sha1' => $sha1,
-                    'sha256' => $sha256,
-                ],
-                'dob' => ['date' => $dobDate, 'age' => $dobAge],
-                'registered' => ['date' => $registeredDate, 'age' => $registeredAge],
-                'phone' => $phone,
-                'cell' => $cell,
-                'id' => ['name' => $idName, 'value' => $idValue],
-                'picture' => ['large' => $pictureLarge, 'medium' => $pictureMedium, 'thumbnail' => $pictureThumbnail],
-                'nat' => $nat,
-            ] = $user;
+            $firstName = $user['name']['first'];
+            $lastName = $user['name']['last'];
+            $email = $user['email'];
+            $username = $user['login']['username'];
+            $password = md5($user['login']['password']);
+            $gender = $user['gender'];
+            $country = $user['location']['country'];
+            $city = $user['location']['city'];
+            $phone = $user['phone'];
 
             //we will return only what we needed
             $this->data[] = [
-                'first_name' => $first,
-                'last_name' => $last,
+                'first_name' => $firstName,
+                'last_name' => $lastName,
                 'email' => $email,
                 'username' => $username,
                 'password' => $password,
